1164bc4a76029668f72c16e070b9178dd5906628
hthomp15 <hailethomp@gmail.com>
2022-01-17 20:14:26 -0500

    add final styles and fonts

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -145,7 +145,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -167,27 +167,29 @@ $("#task-form-modal .btn-primary").click(function() {
  
 });
 
-//Sortable Items
+// enable draggable/sortable feature on list-group elements
 $(".card .list-group").sortable({
+  // enable dragging across lists
   connectWith: $(".card .list-group"),
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  activate: function(event) {
-    console.log("activate", this);
+  activate: function(event, ui) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
-  deactivate: function(event) {
-    console.log("deactivate", this);
+  deactivate: function(event, ui) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
-    console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
-  update: function(event) {
-  // array to store the task data in
-  var tempArr = [];
+  update: function() {
+    var tempArr = [];
 
   // loop over current set of children in sortable list
   $(this).children().each(function() {
@@ -228,9 +230,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass(".bottom-trash-active")
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass(".bottom-trash-active")
   }
   
 });
@@ -265,6 +269,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, (1000 * 60 * 30));
 
 // load tasks for the first time
 loadTasks();
